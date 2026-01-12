c802b8f8df26e1f45ecf7a5b4b599ff70a04eea1
GitCat-glitch <imfishercat@gmail.com>
2026-01-12 17:25:41 +0300

    Update fxloader.lua

--- fxloader.lua
+++ fxloader.lua
@@ -1,16 +1,17 @@
 local gameID = tostring(game.GameId)
 print("hecker_melon was here")
 print(gameID)
+
 local supported_games = {
     "9098443082",
 }
 
 for _, v in ipairs(supported_games) do
     if v == gameID then
-        print("Game is supported!")
+        print("load!")
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/FXSploit/FXHub/refs/heads/main/games/" .. gameID .. ".lua"), true)()
+
         break
     end
 end
-
-
-loadstring("https://raw.githubusercontent.com/FXSploit/FXHub/refs/heads/main/games/".. tostring(gameID).. ".lua", true)()
